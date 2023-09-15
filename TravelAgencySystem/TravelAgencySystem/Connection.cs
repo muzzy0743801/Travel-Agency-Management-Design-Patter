a392c1b6c5dccb52f07d3aa2294a2f953c6d8f77
Muzzamil Waseem <muxxamil0743801@gmail.com>
2023-09-15 21:14:08 +0500

    Travel-Agency-Management-Design-Patter

---
+++ TravelAgencySystem/TravelAgencySystem/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TravelAgencySystem
+{
+    public  class Connection
+    {
+        protected Connection()
+        {
+
+        }
+        static SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-2FC7BGT\SQL;Initial Catalog=TravelAgencySystem;Integrated Security=True");
+        public static SqlConnection GetCon()
+        {
+
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+          
+            return con;
+        }
+    }
+}
